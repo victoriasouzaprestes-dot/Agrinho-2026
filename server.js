97099f671c197c9ba8fba7d1de68891eaa0dc7c9
victoriasouzaprestes-dot <victoria.souza.prestes@escola.pr.gov.br>
2026-04-29 09:42:20 -0300

    alteração 2

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
+const farmRoutes = require('./routes/farmRoutes');  // Importando as rotas
 
 const app = express();
 const port = 3000;
 
+// Middleware para interpretar o corpo da requisição como JSON
+app.use(bodyParser.json());
+
 // Servir arquivos estáticos (o HTML, CSS, JS)
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Rota de teste
+// Usando as rotas da fazenda
+app.use('/api', farmRoutes);
+
+// Rota inicial
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
